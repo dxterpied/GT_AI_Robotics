2654eb24ef2fc628db93a597e5b893e157fad957
iivanov8 <iivanov8@gatech.edu>
2016-05-29 14:02:06 -0400

    saving

--- EV3/test.py
+++ EV3/test.py
@@ -1,15 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
-#from ev3 import
+import ev3dev.ev3 as ev3
+from ev3dev.auto import OUTPUT_A, Motor
 import time
-#
-# m = Motor(OUTPUT_A)
-#
-# m.run_forever(duty_cycle_sp = 100)
 
-print ""
-print ""
-print "hello sucker!"
+m = Motor(OUTPUT_A)
+m.run_forever(duty_cycle_sp = 100)
+time.sleep(1)
+m.stop()
 
-time.sleep(5)
-#m.stop()
+time.sleep(1)
+ev3.Sound.speak('I am done running, thank you!').wait()
+
+
+#m = ev3.LargeMotor('outA')
+#m.run_timed(time_sp=3000, duty_cycle_sp=75)
+
+#ts = ev3.TouchSensor()
+#while True:
+#    ev3.Leds.set_color(ev3.Leds.LEFT, (ev3.Leds.GREEN, ev3.Leds.RED)[ts.value()])
+
+

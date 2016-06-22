c717c31f9b2d4644dea9c736f7fdf766cf5d580e
iivanov8 <iivanov8@gatech.edu>
2016-06-21 20:46:45 -0400

    saving

--- Quizzes/RR3.py
+++ Quizzes/RR3.py
@@ -114,6 +114,81 @@ def demo_grading(hunter_bot, target_bot, next_move_fcn, OTHER = None):
             print "It took too many steps to catch the target."
     return caught
 
+
+def demo_grading_visual(hunter_bot, target_bot, next_move_fcn, OTHER = None):
+    """Returns True if your next_move_fcn successfully guides the hunter_bot
+    to the target_bot. This function is here to help you understand how we
+    will grade your submission."""
+    max_distance = 1.94 * target_bot.distance # 1.94 is an example. It will change.
+    separation_tolerance = 0.02 * target_bot.distance # hunter must be within 0.02 step size to catch target
+    caught = False
+    ctr = 0
+
+    # if you haven't localized the target bot, make a guess about the next
+    # position, then we move the bot and compare your guess to the true
+    # next position. When you are close enough, we stop checking.
+    #For Visualization
+    import turtle    #You need to run this locally to use the turtle module
+    window = turtle.Screen()
+    window.bgcolor('white')
+    size_multiplier= 25.0  #change Size of animation
+    broken_robot = turtle.Turtle()
+    broken_robot.shape('turtle')
+    broken_robot.color('green')
+    broken_robot.resizemode('user')
+    broken_robot.shapesize(0.2, 0.2, 0.2)
+    prediction = turtle.Turtle()
+    prediction.shape('arrow')
+    prediction.color('blue')
+    prediction.resizemode('user')
+    prediction.shapesize(0.2, 0.2, 0.2)
+    #prediction.penup()
+    broken_robot.penup()
+    #End of Visualization
+
+    # We will use your next_move_fcn until we catch the target or time expires.
+    while not caught and ctr < 1000:
+
+        # Check to see if the hunter has caught the target.
+        hunter_position = (hunter_bot.x, hunter_bot.y)
+        target_position = (target_bot.x, target_bot.y)
+        separation = distance_between(hunter_position, target_position)
+        if separation < separation_tolerance:
+            print "You got it right! It took you ", ctr, " steps to catch the target."
+            caught = True
+
+        # The target broadcasts its noisy measurement
+        target_measurement = target_bot.sense()
+
+        # This is where YOUR function will be called.
+        turning, distance, OTHER = next_move_fcn(hunter_position, hunter_bot.heading, target_measurement, max_distance, OTHER)
+
+        # Don't try to move faster than allowed!
+        if distance > max_distance:
+            distance = max_distance
+
+        # We move the hunter according to your instructions
+        hunter_bot.move(turning, distance)
+
+        # The target continues its (nearly) circular motion.
+        target_bot.move_in_circle()
+
+        broken_robot.setheading(target_bot.heading*180/pi)
+        broken_robot.goto(target_bot.x * size_multiplier, target_bot.y * size_multiplier - 200)
+        broken_robot.stamp()
+        prediction.setheading(target_bot.heading*180/pi)
+        prediction.goto(hunter_bot.x * size_multiplier, hunter_bot.y * size_multiplier - 200)
+        prediction.stamp()
+
+        ctr += 1
+        if ctr >= 1000:
+            print "It took too many steps to catch the target."
+
+
+
+    return caught
+
+
 def angle_trunc(a):
     """This maps all angles to a domain of [-pi, pi]"""
     while a < 0.0:
@@ -128,36 +203,14 @@ def get_heading(hunter_position, target_position):
     heading = angle_trunc(heading)
     return heading
 
-def naive_next_move(hunter_position, hunter_heading, target_measurement, max_distance, OTHER):
-    """This strategy always tries to steer the hunter directly towards where the target last
-    said it was and then moves forwards at full speed. This strategy also keeps track of all
-    the target measurements, hunter positions, and hunter headings over time, but it doesn't
-    do anything with that information."""
-    if not OTHER: # first time calling this function, set up my OTHER variables.
-        measurements = [target_measurement]
-        hunter_positions = [hunter_position]
-        hunter_headings = [hunter_heading]
-        OTHER = (measurements, hunter_positions, hunter_headings) # now I can keep track of history
-    else: # not the first time, update my history
-        OTHER[0].append(target_measurement)
-        OTHER[1].append(hunter_position)
-        OTHER[2].append(hunter_heading)
-        measurements, hunter_positions, hunter_headings = OTHER # now I can always refer to these variables
-
-    heading_to_target = get_heading(hunter_position, target_measurement)
-    heading_difference = heading_to_target - hunter_heading
-    turning =  heading_difference # turn towards the target
-    distance = max_distance # full speed ahead!
-    return turning, distance, OTHER
-
-target = robot(0.0, 10.0, 0.0, 2*pi / 30, 1.5)
+target = robot(0.0, 0.0, 0.0, 2*pi / 30, 1.5)
 measurement_noise = .05*target.distance
 target.set_noise(0.0, 0.0, measurement_noise)
 
-hunter = robot(-10.0, -10.0, 0.0)
+hunter = robot(-10.0, -20.0, 0.0)
 
-print demo_grading(hunter, target, next_move)
-
+#demo_grading(hunter, target, next_move)
+demo_grading_visual(hunter, target, next_move)
 
 
 

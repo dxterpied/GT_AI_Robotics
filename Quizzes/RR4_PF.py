22a6c4be270cbfe05395a9c418072a80debf58a1
iivanov8 <iivanov8@gatech.edu>
2016-07-05 19:10:17 -0400

    saving

--- Quizzes/RR4_PF.py
+++ Quizzes/RR4_PF.py
@@ -27,13 +27,11 @@ hunter = robot(-10.0, -10.0, 0.0)
 
 
 def next_move_cut_angle(hunter_position, hunter_heading, target_measurement, max_distance, OTHER = None):
-
-
-
     #time.sleep(0.5)
     xy_estimate = target_measurement
     xy_estimate_first_step = target_measurement
     xy_estimate_second_step = target_measurement
+    xy_estimate_third_step = target_measurement
 
     if OTHER is None:
         distances = []
@@ -43,10 +41,7 @@ def next_move_cut_angle(hunter_position, hunter_heading, target_measurement, max
         distances, angles, coords = OTHER
 
         if len(coords) == 1:
-            x1, y1 = coords[0]
-            x2, y2 = target_measurement
             hypotenuse1 = distance_between(coords[0], target_measurement)
-            y1Delta = y2 - y1
             distances.append(hypotenuse1)
 
         elif len(coords) >= 2:
@@ -71,19 +66,25 @@ def next_move_cut_angle(hunter_position, hunter_heading, target_measurement, max
             avgDT = sum(distances)/len(distances)
             avgAngle = sum(angles)/len(angles)
 
-            newR = robot(point3[0], point3[1], headingAngle2, avgAngle, avgDT)
+            # create particles only after approximate turning and distance are known
+            if len(particles) == 0:
+                createParticles(target_measurement[0], target_measurement[1], avgAngle, avgDT)
+
+            Z = senseToLandmarks(target_measurement[0], target_measurement[1])
+            xy_PF = particle_filter(Z, avgAngle, avgDT)
+
+            newR = robot(xy_PF[0], xy_PF[1], headingAngle2, avgAngle, avgDT)
             newR.move_in_circle()
             xy_estimate_first_step = newR.x, newR.y
             newR.move_in_circle()
             xy_estimate_second_step = newR.x, newR.y
             newR.move_in_circle()
-
-            xy_estimate = newR.x, newR.y
+            xy_estimate_third_step = newR.x, newR.y
 
     coords.append(target_measurement)
     OTHER = (distances, angles, coords)
 
-    distance = distance_between(hunter_position, xy_estimate)
+    distance_third_step = distance_between(hunter_position, xy_estimate)
     distance_first_step = distance_between(hunter_position, xy_estimate_first_step)
     distance_second_step = distance_between(hunter_position, xy_estimate_second_step)
 
@@ -91,11 +92,15 @@ def next_move_cut_angle(hunter_position, hunter_heading, target_measurement, max
     if distance_first_step < max_distance:
         distance = distance_first_step
         xy_estimate = xy_estimate_first_step
-
     # if second step prediction is within hunter's reach, go there
-    if distance_second_step < max_distance:
+    elif distance_second_step < max_distance:
         distance = distance_second_step
         xy_estimate = xy_estimate_second_step
+    # else head to three steps forward
+    else:
+        distance = distance_third_step
+        xy_estimate = xy_estimate_third_step
+
 
     heading_to_target = get_heading(hunter_position, xy_estimate)
     turning = heading_to_target - hunter_heading # turn towards the target
@@ -168,49 +173,69 @@ def next_move_straight_line(hunter_position, hunter_heading, target_measurement,
 
             #xy_estimate = particle_filter(Z, avgAngle, avgDT)
 
-            #newR = robot(point3[0], point3[1], headingAngle2, avgAngle, avgDT)
+            #newR = robot(xy_estimate[0], xy_estimate[1], headingAngle2, avgAngle, avgDT)
             newR = robot(xy_PF[0], xy_PF[1], headingAngle2, avgAngle, avgDT)
 
-            newR.move_in_circle()
-            predictedPosition = newR.x, newR.y
+            broken_robot = turtle.Turtle()
+            broken_robot.shape('circle')
+            broken_robot.color('black')
+            broken_robot.penup()
+            broken_robot.shapesize(0.4, 0.4, 0.2)
 
-            if xy_estimate is None:
+            broken_robot.goto(xy_PF[0] * size_multiplier, xy_PF[1] * size_multiplier - 200)
+            broken_robot.stamp()
 
-                # broken_robot = turtle.Turtle()
-                # broken_robot.shape('turtle')
-                # broken_robot.color('red')
-                # #broken_robot.resizemode('user')
-                # broken_robot.shapesize(0.2, 0.2, 0.2)
 
-                steps = 1
+            #newR.move_in_circle()
+            #predictedPosition = newR.x, newR.y
 
-                while True:
-                    #time.sleep(0.1)
-                    xy_estimate = newR.x, newR.y
-                    headingAngle2 = newR.heading
-                    distanceBetweenHunterAndRobot = distance_between(hunter_position, xy_estimate)
-                    # check how many steps it will take to get there for Hunter
-                    projectedDistance = steps * max_distance
-
-                    # broken_robot.setheading(headingAngle2 * 180/pi)
-                    # broken_robot.goto(newR.x * 25, newR.y * 25 - 200)
-                    # broken_robot.stamp()
-
-                    if projectedDistance >= distanceBetweenHunterAndRobot:
-                        #print xy_estimate, steps
-                        break
-
-                    steps += 1
-                    if steps > 50:
-                        break
-
-                    newR.move_in_circle()
-
-            else:
-                steps -= 1
-                #print "decrement steps", steps
-                if steps <= 0:
-                    xy_estimate = None
+            # if xy_estimate is None:
+            #
+            #     broken_robot = turtle.Turtle()
+            #     broken_robot.shape('circle')
+            #     broken_robot.color('black')
+            #     broken_robot.shapesize(0.4, 0.4, 0.2)
+            #
+            #     broken_robot.goto(xy_PF[0] * size_multiplier, xy_PF[1] * size_multiplier - 200)
+            #     broken_robot.stamp()
+            #
+            #     broken_robot.color('red')
+            #     broken_robot.shapesize(0.2, 0.2, 0.2)
+            #
+            #
+            #     steps = 1
+            #
+            #     counter = 1
+            #     while True:
+            #         #time.sleep(0.1)
+            #         xy_estimate = newR.x, newR.y
+            #         headingAngle2 = newR.heading
+            #         distanceBetweenHunterAndRobot = distance_between(hunter_position, xy_estimate)
+            #         # check how many steps it will take to get there for Hunter
+            #         projectedDistance = steps * max_distance
+            #
+            #         broken_robot.goto(newR.x * size_multiplier, newR.y * size_multiplier - 200)
+            #         broken_robot.stamp()
+            #
+            #         if projectedDistance >= distanceBetweenHunterAndRobot:
+            #             #print xy_estimate, steps
+            #             break
+            #
+            #         steps += 1
+            #         counter += 1
+            #
+            #         if counter > 2:
+            #             break
+            #         if steps > 50:
+            #             break
+            #
+            #         newR.move_in_circle()
+            #
+            # else:
+            #     steps -= 1
+            #     #print "decrement steps", steps
+            #     if steps <= 0:
+            #         xy_estimate = None
 
     coords.append(target_measurement)
     OTHER = (distances, angles, coords, xy_estimate, steps)
@@ -221,6 +246,8 @@ def next_move_straight_line(hunter_position, hunter_heading, target_measurement,
 
     heading_to_target = get_heading(hunter_position, xy_estimate)
     heading_to_target2 = get_heading(hunter_position, predictedPosition)
+    distance = distance_between(hunter_position, xy_estimate)
+    distance2 = distance_between(hunter_position, predictedPosition)
 
     turning = heading_to_target - hunter_heading # turn towards the target
     if abs(turning) > pi:
@@ -228,28 +255,11 @@ def next_move_straight_line(hunter_position, hunter_heading, target_measurement,
 
     turning2 = heading_to_target2 - hunter_heading # turn towards the target
 
-    # if heading_to_target < -2.0 and hunter_heading > 0:
-    #     print heading_to_target, hunter_heading, turning
-    #     time.sleep(2)
-
-
-    #print hunter_position, xy_estimate, heading_to_target, hunter_heading, turning
-    distance = distance_between(hunter_position, xy_estimate)
-    distance2 = distance_between(hunter_position, predictedPosition)
 
     if distance2 <= max_distance:
         turning = turning2
         distance = distance2
         OTHER = (distances, angles, coords, None, steps)
-
-
-    # if steps == 1 and distance2 < distance:
-    #      turning = turning2
-    #      distance = distance2
-
-    #turning = turning2
-    #distance = distance2
-
 
     #print turning, distance
     return turning, distance, OTHER
@@ -343,7 +353,6 @@ def demo_grading_visual(hunter_bot, target_bot, next_move_fcn, OTHER = None):
 
     #window = turtle.Screen()
     #window.bgcolor('white')
-    size_multiplier= 20.0  #change Size of animation
     broken_robot = turtle.Turtle()
     broken_robot.shape('turtle')
     broken_robot.color('green')
@@ -409,6 +418,14 @@ def demo_grading_visual(hunter_bot, target_bot, next_move_fcn, OTHER = None):
 
         ctr += 1
         if ctr >= 1000:
+
+            "printing predictions"
+            prediction.color('red')
+            for i in range(N):
+                p = particles[i]
+                prediction.goto(p.x * size_multiplier, p.y  *size_multiplier-200)
+                prediction.stamp()
+
             print "It took too many steps to catch the target."
 
 
@@ -567,7 +584,7 @@ demo_grading_visual(hunter, target, next_move_straight_line)
 # print "maximum score: ", max(scores)
 # print "fails: ", fails
 
-
+turtle.getscreen()._root.mainloop()
 
 
 

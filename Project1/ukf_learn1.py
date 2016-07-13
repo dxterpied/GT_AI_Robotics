607788f2f7d33a2f6f2e44e0da8ba8a958711d5e
ilyaivanov07 <ilyaivanov07@gmail.com>
2016-07-13 14:35:24 -0400

    saving

--- Project1/ukf_learn1.py
+++ Project1/ukf_learn1.py
@@ -21,7 +21,26 @@ UKF; and finally the same given by the UKS.
 '''
 import numpy as np
 import pylab as pl
-from pykalman import UnscentedKalmanFilter
+from UKF import *
+import turtle
+from math import *
+import random
+
+turtle.setup(800, 800)
+window = turtle.Screen()
+window.bgcolor('white')
+
+target_robot = turtle.Turtle()
+target_robot.shape('turtle')
+target_robot.color('green')
+target_robot.shapesize(0.2, 0.2, 0.2)
+
+predicted_robot = turtle.Turtle()
+predicted_robot.shape('circle')
+predicted_robot.color('blue')
+predicted_robot.shapesize(0.2, 0.2, 0.2)
+
+size_multiplier = 25
 
 # F matrix - next state function
 def transition_function(state, noise):
@@ -35,48 +54,120 @@ def observation_function(state, noise):
     return np.dot(C, state) + noise
 
 
+
 transition_covariance = np.eye(2)  # identity matrix
 # [[ 1.  0.]
 #  [ 0.  1.]]
-
 random_state = np.random.RandomState(0)
 measurement_uncertainty = np.eye(2) + random_state.randn(2, 2) * 0.1  # R matrix;  measurement uncertainty
 # [[ 1.17640523  0.04001572]
 #  [ 0.0978738   1.22408932]]
-
 initial_state_mean = [0, 0] # x matrix  # state matrix
-
 initial_state_covariance = [[1, 0.1],
                             [-0.1, 1]]
 
+def angle_trunc(a):
+    while a < 0.0:
+        a += pi * 2
+    return ((a + pi) % (pi * 2)) - pi
+
+class robot:
+
+    def __init__(self, x = 0.0, y = 0.0, heading = 0.0, turning = 2*pi/10, distance = 1.0):
+        self.x = x
+        self.y = y
+        self.heading = heading
+        self.turning = turning # only applies to target robots who constantly move in a circle
+        self.distance = distance # only applies to target bot, who always moves at same speed.
+        self.turning_noise    = 0.0
+        self.distance_noise    = 0.0
+        self.measurement_noise = 0.0
+
+
+    def set_noise(self, new_t_noise, new_d_noise, new_m_noise):
+        """This lets us change the noise parameters, which can be very
+        helpful when using particle filters."""
+        self.turning_noise    = float(new_t_noise)
+        self.distance_noise    = float(new_d_noise)
+        self.measurement_noise = float(new_m_noise)
+
+    def moveInCircle(self):
+        # Execute motion
+        self.heading += self.turning # update the angle to create a new angle
+        self.heading = angle_trunc(self.heading)
+        self.x += self.distance * cos(self.heading)
+        self.y += self.distance * sin(self.heading)
+
+    def moveStraight(self):
+        self.x += self.distance
+        self.y += self.distance/2.0
+
+    def sense(self):
+        """This function represents the robot sensing its location. When
+        measurements are noisy, this will return a value that is close to,
+        but not necessarily equal to, the robot's (x, y) position."""
+        return (random.gauss(self.x, self.measurement_noise),
+                random.gauss(self.y, self.measurement_noise))
+
+    def __repr__(self):
+        """This allows us to print a robot's position"""
+        return '[%.5f, %.5f]'  % (self.x, self.y)
+
+
+def generateStatesAndObservations():
+    states = []
+    observations = []
+
+    target = robot(-10.0, -0.0, 0.0, 2*pi / 30, 1)
+    target.set_noise(0.0, 0.0, 0.2)
+
+
+    for i in range(10):
+        target.moveInCircle()
+        states.append((target.x, target.y))
+        observations.append(target.sense())
+
+    return (states, observations)
+
+
+states = [[0., 0.], [2. ,2.], [5., 5.]]
+observations = [[0.1, 0.2], [2.1 ,2.2], [5.0,5.1]]
+
+
+
 # sample from model
-kf = UnscentedKalmanFilter(
+kf = UKF(
     transition_function, observation_function,
     transition_covariance, measurement_uncertainty,
     initial_state_mean, initial_state_covariance,
     random_state=random_state
 )
 
-states = [[0., 0.], [2. ,2.], [5., 5.]]
-observations = [[0.1, 0.2], [2.1 ,2.2], [5.0,5.1]]
-#states, observations = kf.sample(50, initial_state_mean)
-
-#print states, observations
-
+#states, observations = kf.sample(20, initial_state_mean)
+states, observations = generateStatesAndObservations()
 # estimate state with filtering and smoothing
+
 filtered_state_estimates = kf.filter(observations)[0]
 
-#print filtered_state_estimates
+# print filtered_state_estimates
+# print states
 
-# draw estimates
+
+for i in range(len(filtered_state_estimates)):
+    target_robot.goto(states[i][0] * size_multiplier, states[i][1] * size_multiplier)
+    target_robot.stamp()
+
+    predicted_robot.goto(filtered_state_estimates[i][0] * size_multiplier,filtered_state_estimates[i][1] * size_multiplier)
+    predicted_robot.stamp()
+
+
+
+#draw estimates
 pl.figure()
 lines_true = pl.plot(states, color='b')
 lines_filt = pl.plot(filtered_state_estimates, color='r', ls='-')
+pl.legend((lines_true[0], lines_filt[0]), ('actual data', 'UKF'), loc='lower left')
+pl.show()
 
+turtle.getscreen()._root.mainloop()
 
-pl.legend((lines_true[0], lines_filt[0]),
-          ('actual data', 'UKF'),
-          loc='lower left')
-
-
-#pl.show()
--- Quizzes/RR5.py
+++ Quizzes/RR5.py
@@ -424,7 +424,7 @@ def demo_grading(hunter_bot, target_bot, next_move_fcn, OTHER = None):
     """Returns True if your next_move_fcn successfully guides the hunter_bot
     to the target_bot. This function is here to help you understand how we
     will grade your submission."""
-    max_distance = 0.98 * target_bot.distance # 0.98 is an example. It will change.
+    max_distance = 0.97 * target_bot.distance # 0.97 is an example. It will change.
     separation_tolerance = 0.02 * target_bot.distance # hunter must be within 0.02 step size to catch target
     caught = False
     ctr = 0
@@ -465,27 +465,27 @@ def demo_grading(hunter_bot, target_bot, next_move_fcn, OTHER = None):
     return caught
 
 
-demo_grading_visual(hunter, target, next_move_straight_line)
+#demo_grading_visual(hunter, target, next_move_straight_line)
 #demo_grading(hunter, target, next_move_straight_line)
 
-# scores = []
-# fails = 0
-# for i in range(1000):
-#     print i
-#     particles = []
-#     target = robot(0.0, 10.0, 0.0, 2*pi / 30, 1.5)
-#     target.set_noise(0.0, 0.0, .05*target.distance)
-#     hunter = robot(-10.0, -10.0, 0.0)
-#     score = demo_grading(hunter, target, next_move_straight_line)
-#     if score == 1000:
-#         fails += 1
-#     else:
-#         scores.append(score)
-#
-# print "average score: ", sum(scores)/ float(len(scores))
-# print "minimum score: ", min(scores)
-# print "maximum score: ", max(scores)
-# print "fails: ", fails
+scores = []
+fails = 0
+for i in range(1000):
+    print i
+    particles = []
+    target = robot(0.0, 10.0, 0.0, 2*pi / 30, 1.5)
+    target.set_noise(0.0, 0.0, 2.0 * target.distance)
+    hunter = robot(-10.0, -10.0, 0.0)
+    score = demo_grading(hunter, target, next_move_straight_line)
+    if score == 1000:
+        fails += 1
+    else:
+        scores.append(score)
+
+print "average score: ", sum(scores)/ float(len(scores))
+print "minimum score: ", min(scores)
+print "maximum score: ", max(scores)
+print "fails: ", fails
 #
 # with predicted angle:
 # average score:  165.305
